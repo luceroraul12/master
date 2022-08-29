7b15533a718c5e184cd02c5aafad0ac6c6678c53
Lucero Raul <luceroraul12@gmail.com>
2022-08-29 17:35:24 -0300

    delete- router-outlet

--- src/app/components/formulario/formulario-factura/formulario-factura.component.ts
+++ src/app/components/formulario/formulario-factura/formulario-factura.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Factura } from 'src/app/models/factura';
 
 @Component({
   selector: 'app-formulario-factura',
@@ -7,13 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioFacturaComponent implements OnInit {
 
-  constructor() { }
+  public factura: Factura = {} as Factura;
+
+
+  constructor() {}
 
   ngOnInit(): void {
   }
 
   enviarDatos(){
-    alert("INFORMACION ENVIADA");
+    alert("INFORMACION ENVIADA: "+ this.factura.id + " " + this.factura.nombre);
   }
 
 }
