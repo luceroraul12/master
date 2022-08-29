dac77cff8974ddf582e980dd41680de42f0906ff
Lucero Raul <luceroraul12@gmail.com>
2022-08-29 14:14:58 -0300

    add. dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +13,8 @@ import { PagoComponent } from './components/pago/pago.component';
 import { ResumenComponent } from './components/resumen/resumen.component';
 import { BarraMenuComponent } from './components/barra-menu/barra-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormularioFacturaComponent } from './components/formulario/formulario-factura/formulario-factura.component';
+import { FormularioPagoComponent } from './components/formulario/formulario-pago/formulario-pago.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +22,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FacturaComponent,
     PagoComponent,
     ResumenComponent,
-    BarraMenuComponent
+    BarraMenuComponent,
+    FormularioFacturaComponent,
+    FormularioPagoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
