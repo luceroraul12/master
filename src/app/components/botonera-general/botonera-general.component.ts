728e43029234a95865eab72ded0f0c62fe03bd26
Lucero Raul <luceroraul12@gmail.com>
2022-09-02 10:25:44 -0300

    boton crear en funcion factura pago

--- src/app/components/botonera-general/botonera-general.component.ts
+++ src/app/components/botonera-general/botonera-general.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { FormularioFacturaComponent } from '../formulario/formulario-factura/formulario-factura.component';
+import { FormularioPagoComponent } from '../formulario/formulario-pago/formulario-pago.component';
 import { FormularioResumenComponent } from '../formulario/formulario-resumen/formulario-resumen.component';
 
 @Component({
@@ -8,6 +10,8 @@ import { FormularioResumenComponent } from '../formulario/formulario-resumen/for
   styleUrls: ['./botonera-general.component.scss']
 })
 export class BotoneraGeneralComponent implements OnInit {
+
+  @Input() tipo!: string;
 
   constructor(public dialog: MatDialog) { }
 
@@ -24,4 +28,38 @@ export class BotoneraGeneralComponent implements OnInit {
     });
   }
 
+crear(){
+  this.adaptarTipoyCrear();
+}
+
+  adaptarTipoyCrear(): void{
+    switch(this.tipo){
+      case "factura":
+        this.crearFactura();
+        break;
+      case "pago":
+        this.crearPago();
+        break;
+    }
+  }
+
+  crearFactura(){
+    const dialogRef = this.dialog.open(FormularioFacturaComponent, {
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  crearPago(){
+    const dialogRef = this.dialog.open(FormularioPagoComponent, {
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
