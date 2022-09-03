a658d062684f4b0ac1977658945c8ee6267dd29e
Lucero Raul <luceroraul12@gmail.com>
2022-09-03 00:13:37 -0300

    add info ejemplo

--- src/app/services/mensualidad.service.ts
+++ src/app/services/mensualidad.service.ts
@@ -7,8 +7,31 @@ import { Pago } from '../models/pago';
 })
 export class MensualidadService {
 
-  private _facturasTotales: Factura[] = [];
-  private _pagosTotales: Pago[] = [];
+  private _facturasTotales: Factura[] = [
+    {
+      id: 1,
+      nombre: "movistar",
+      url: "www.movistar.com"
+    },
+    {
+      id: 2,
+      nombre: "supercanal",
+      url: "www.supercanal.com"
+    },
+    {
+      id: 3,
+      nombre: "naranja",
+      url: "www.naranja.com"
+    }
+  ];
+  private _pagosTotales: Pago[] = [
+    {
+      id: 1,
+      pago: 123.24,
+      fechaDePago: new Date("03/05/2022"),
+      factura: this._facturasTotales[0]
+    }
+  ];
 
   get facturasTotales(){
     return [...this._facturasTotales];
